8946520a12cc61c1c88560fa44c1338559911b18
opielapatryk <104018084+opielapatryk@users.noreply.github.com>
2022-10-13 14:42:36 +0200

    e

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,28 +7,27 @@ function AppNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">GPELEKTRO</Navbar.Brand>
+        <Navbar.Brand href="#home">Gp - Electro Automatic</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#features">About us</Nav.Link>
+            <NavDropdown title="Pricing" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Sick</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Omron
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">PC</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Something else
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link href="#deets">RODO</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+              Contact
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
