9a8309f0394d209895dca85019bc5d48b4203014
opielapatryk <104018084+opielapatryk@users.noreply.github.com>
2022-10-14 08:20:25 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,37 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function AppNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Gp - Electro Automatic</Navbar.Brand>
+        <Navbar.Brand href="#home" style={{letterSpacing: 2}}>Gp - Electro Automatic</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">About us</Nav.Link>
-            <NavDropdown title="Pricing" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Sick</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Omron
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">PC</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Something else
-              </NavDropdown.Item>
+        <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-danger">Search</Button>
+          </Form>
+          <Nav className="ms-auto">
+            <Nav.Link href="#aboutus">About us</Nav.Link>
+            <Nav.Link href="#products">Products</Nav.Link>
+            <Nav.Link href="#rodo">RODO</Nav.Link>
+            <Nav.Link href="#contacts">Contact</Nav.Link>
+            <NavDropdown title="Language" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#English">English</NavDropdown.Item>
+              <NavDropdown.Item href="#Polski">Polski</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Nav>
-            <Nav.Link href="#deets">RODO</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Contact
-            </Nav.Link>
-          </Nav>
+          
         </Navbar.Collapse>
       </Container>
     </Navbar>
