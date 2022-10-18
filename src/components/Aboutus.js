407e49683e413acf69e169b4bde756d1cb37abb5
opielapatryk <104018084+opielapatryk@users.noreply.github.com>
2022-10-18 09:19:18 +0200

    about us update

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -4,7 +4,29 @@ function Aboutus(){
     return(
         <>
         <AppNavBar/>
-         <h1>About Us!</h1>
+        <div style={{color:"white", marginLeft:"3%", minHeight:"100vh"}}>
+        <h1>About us! </h1>
+         <h5>Lorem Ipsum</h5>
+         <p >Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, <br></br>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium sagittis velit, id facilisis metus auctor vel. Duis venenatis arcu ut facilisis tempor :</p>
+
+         <h5>LOREM IPSUM</h5>
+         <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium sagittis velit, id facilisis metus auctor vel. Duis venenatis arcu ut facilisis tempor. Vivamus molestie, sapien eget ultrices tempor, neque leo fringilla ex, id porttitor nulla ligula quis velit. Morbi feugiat posuere enim. Ut id augue sagittis, mattis nibh cursus, placerat velit. Nam eleifend finibus nisi. Ut quis tincidunt nulla. Vestibulum eu lectus vel lacus commodo accumsan at eu felis. Phasellus nec pharetra tortor, semper volutpat leo. Donec ut mauris tincidunt, convallis metus eget, dictum lacus. Aliquam ut metus magna.</p><br></br>
+         <p >Neque porro quisquam :</p>
+        <ul>
+            <li >Lorem ipsum dolor sit amet,</li>
+            <li >Suspendisse pretium sagittis velit, id facilisis metus auctor vel. Duis venenatis arcu ut facilisis tempor.</li>
+            <li >Vivamus molestie, sapien eget ultrices tempor, neque leo fringilla ex, id porttitor nulla ligula quis velit.</li>
+            <li >Nam eleifend finibus nisi. Ut quis tincidunt nulla. Vestibulum eu lectus vel lacus commodo accumsan at eu felis.</li>
+            <li >Phasellus nec pharetra tortor, semper volutpat leo. Donec ut mauris tincidunt, convallis metus eget, dictum lacus. Aliquam ut metus magna.</li>
+        </ul>
+        <p >Duis venenatis arcu ut facilisis tempor. Vivamus molestie, sapien eget ultrices tempor, neque leo fringilla ex, id porttitor nulla ligula quis velit. Morbi feugiat posuere enim. Ut id augue sagittis, mattis nibh cursus, placerat velit.</p>
+        <p >Phasellus nec pharetra tortor, semper volutpat leo. Donec ut mauris tincidunt, convallis metus eget, dictum lacus.
+</p>
+        <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur: ............
+
+</p>
+        </div>
         </>
     )
 }
