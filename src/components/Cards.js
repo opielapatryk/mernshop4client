a04da804974f0d8f8eab6db5b9ff789abfa973a8
opielapatryk <104018084+opielapatryk@users.noreply.github.com>
2022-10-19 11:39:02 +0200

    Product search fix style

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,11 +2,24 @@ import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Data from "../mock-data.json"
 import {useState} from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useTranslation } from 'react-i18next';
 function AppCard() {
   const [query, setQuery] = useState("")
+  const {t} = useTranslation()
   return (
     <>
-    <input placeholder="Enter Post Title" onChange={event => setQuery(event.target.value)} />
+    <Form className="d-flex" style={{margin:"1%"}}>
+            <Form.Control
+              type="search"
+              placeholder={t("Enter Product Code")}
+              className="me-2"
+              aria-label="Search"
+              onChange={event => setQuery(event.target.value)}
+            />
+            <Button variant="outline-danger">{t("Search")}</Button>
+          </Form>
 {
   Data.filter(post => {
     if (query === '') {
@@ -17,13 +30,12 @@ function AppCard() {
   }).map((post, index) => (
   <CardGroup>
       <Card  key={index}>
-        <Card.Img variant="top" src={post.img} style={{width:"20%"}}/>
+        <Card.Img variant="top" src={post.img} style={{width:"20%",paddingTop:"1%",paddingLeft:"1%"}}/>
         <Card.Body>
           <Card.Title>Code: {post.code}</Card.Title>
-          <Card.Title>Price: {post.price}</Card.Title>
         </Card.Body>
         <Card.Footer >
-          <small className="text-muted">999$</small>
+        <Card.Title>Price: {post.price}</Card.Title>
         </Card.Footer>
       </Card>
       
