b5f61db799df312d98caaafee5e5e7287f2c9275
opielapatryk <104018084+opielapatryk@users.noreply.github.com>
2022-10-19 11:28:58 +0200

    search products

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,49 +1,36 @@
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Data from "../mock-data.json"
+import {useState} from "react";
 function AppCard() {
+  const [query, setQuery] = useState("")
   return (
-    <CardGroup>
-      <Card>
-        <Card.Img variant="top" src='./images/3p b 10.jpg' />
+    <>
+    <input placeholder="Enter Post Title" onChange={event => setQuery(event.target.value)} />
+{
+  Data.filter(post => {
+    if (query === '') {
+      return post;
+    } else if (post.code.toLowerCase().includes(query.toLowerCase())) {
+      return post;
+    }
+  }).map((post, index) => (
+  <CardGroup>
+      <Card  key={index}>
+        <Card.Img variant="top" src={post.img} style={{width:"20%"}}/>
         <Card.Body>
-          <Card.Title>Legrand</Card.Title>
-          <Card.Title>3p b 10</Card.Title>
+          <Card.Title>Code: {post.code}</Card.Title>
+          <Card.Title>Price: {post.price}</Card.Title>
         </Card.Body>
         <Card.Footer >
           <small className="text-muted">999$</small>
         </Card.Footer>
       </Card>
-      <Card>
-        <Card.Img variant="top" src='./images/a9d56610.jpg'/>
-        <Card.Body>
-          <Card.Title>Schneider Electric</Card.Title>
-          <Card.Title>a9d56610</Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">777$</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src='./images/ad-atm60-ka3pr.jpg'/>
-        <Card.Body>
-          <Card.Title>Sick</Card.Title>
-          <Card.Title>adatm60ka3pr</Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">927$</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src='./images/bim-unt-ap6x.jpg'/>
-        <Card.Body>
-          <Card.Title>Turck</Card.Title>
-          <Card.Title>bimuntap6</Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">2913$</small>
-        </Card.Footer>
-      </Card>
+      
     </CardGroup>
+    ))
+}
+    </>
   );
 }
 
